1354051062ca981bf916ff6f2c6a4a7e89479b12
FooTearth <footearth@gmail.com>
2019-07-03 18:57:59 +0800

    add all in one script

---
+++ server/all.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash 
+
+# curl -SL https://raw.githubusercontent.com/Mooxe000/mooxe-docker-node/master/server/all.sh | bash
+
+curl -SL https://raw.githubusercontent.com/Mooxe000/mooxe-docker-base/master/server/init.sh | bash
+curl -SL https://raw.githubusercontent.com/Mooxe000/mooxe-docker-base/master/server/docekr.sh | bash
+curl -SL https://raw.githubusercontent.com/Mooxe000/mooxe-docker-node/master/server/node.sh | bash
