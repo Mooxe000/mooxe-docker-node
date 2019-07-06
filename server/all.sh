1f2e540385d1442ceb5a47f0300b67e8c8782c36
FooTearth <footearth@gmail.com>
2019-07-06 13:26:03 +0800

    fix

--- server/all.sh
+++ server/all.sh
@@ -3,5 +3,5 @@
 # curl -SL https://raw.githubusercontent.com/Mooxe000/mooxe-docker-node/master/server/all.sh | bash
 
 curl -SL https://raw.githubusercontent.com/Mooxe000/mooxe-docker-base/master/server/init.sh | bash
-curl -SL https://raw.githubusercontent.com/Mooxe000/mooxe-docker-base/master/server/docekr.sh | bash
+curl -SL https://raw.githubusercontent.com/Mooxe000/mooxe-docker-base/master/server/docker.sh | bash
 curl -SL https://raw.githubusercontent.com/Mooxe000/mooxe-docker-node/master/server/node.sh | bash
