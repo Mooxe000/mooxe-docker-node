211f1aaa9c8ba69f214abe6038c4cb6773ab9b36
FooTearth <footearth@gmail.com>
2019-12-13 00:14:53 +0800

    adjust for node.sh

--- server/node.sh
+++ server/node.sh
@@ -9,8 +9,8 @@ echo ". ~/.nvm/nvm.sh" >> ~/.zshrc
 
 fish -lc "omf i bass nvm"
 
-NODE_VERSION_LTS 12.13.1
-NODE_VERSION 13.3.0
+NODE_VERSION_LTS=12.13.1
+NODE_VERSION=13.3.0
 
 cp -f ~/.nvm/nvm.sh ~/.nvm/nvm-tmp.sh && \
 echo "nvm install v${NODE_VERSION_LTS}" >> ~/.nvm/nvm-tmp.sh && \
