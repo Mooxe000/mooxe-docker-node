e04107a66eb306dcfa44e2447af1c8080ac576f1
FooTearth <footearth@gmail.com>
2020-07-29 17:26:04 +0800

    add test  framework

---
+++ yarn2/my-swc-node/source/test/index.ts
@@ -0,0 +1,9 @@
+import * as fc from 'fast-check'
+import * as laws from 'fp-ts-laws'
+import { eqNumber } from 'fp-ts/lib/Eq'
+
+describe('eq', () => {
+  it('should test Eq laws', () => {
+    laws.eq(eqNumber, fc.float())
+  })
+})
