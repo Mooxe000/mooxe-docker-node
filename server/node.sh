9d0d5572ebd36582fd53a9d1a42af899408a7bc9
footearth <footearth@gmail.com>
2021-01-21 02:53:08 +0800

    update to 14.15.4 & 15.6.0

--- server/node.sh
+++ server/node.sh
@@ -9,8 +9,8 @@ echo ". ~/.nvm/nvm.sh" >> ~/.zshrc
 
 fish -lc "omf i bass nvm"
 
-NODE_VERSION_LTS=14.15.3
-NODE_VERSION=15.5.0
+NODE_VERSION_LTS=14.15.4
+NODE_VERSION=15.6.0
 
 cp -f ~/.nvm/nvm.sh ~/.nvm/nvm-tmp.sh && \
 echo "nvm install v${NODE_VERSION_LTS}" >> ~/.nvm/nvm-tmp.sh && \
